0fe6ec2ddf2c8a5deb19c5bc020b5964c238b53d
mmarian <mihailmarian12@gmail.com>
2024-06-01 21:19:56 +0100

    domain switched to learningquant.com

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_SOCIAL_IMAGE = "/meta.webp";
-export const SITE_TITLE = "The Sceptical Quant";
+export const SITE_TITLE = "LearningQuant.com";
